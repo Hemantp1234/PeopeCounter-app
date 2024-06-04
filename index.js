75a3fca7c3dde0ccfd9fd540507b4ce15d38ead3
unknown <patihemant856@gmail.com>
2024-06-04 12:03:13 +0530

    adding all

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count=0
+function increment()
+{
+    count +=1
+    countEl.textContent=count
+}
+
+function save()
+{
+    let countStr = count + "-"
+    saveEl.textContent += countStr
+    countEl.textContent= 0
+    count= 0
+
+}
+
+function zero()
+{
+    countStr=0
+    saveEl.textContent=0
+    
+}
+
+console.log("hemant patil")
